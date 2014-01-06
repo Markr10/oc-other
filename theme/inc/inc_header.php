60529bc2ef66a6e274a2161a44511c689d6a0375
RonSmit <ronsmit92@gmail.com>
2014-01-06 12:08:27 +0100

    De juiste URL's toegevoegd

--- theme/inc/inc_header.php
+++ theme/inc/inc_header.php
@@ -3,9 +3,9 @@ echo '<header id="header">
                 <div class="menuspacer">
                     <a href="index.php"><div id="myconcept"></div></a>
                     <a href="index.php"><div class="menuitem">Home</div></a>
-                    <a href="onderwijs.html"><div class="menuitem">Onderwijs</div></a>
-                    <a href="myconcept.html"><div class="menuitem">MyConcept</div></a>
-                    <a href="media.html"><div class="menuitem">Media</div></a>
+                    <a href="onderwijs.php"><div class="menuitem">Onderwijs</div></a>
+                    <a href="myconcept.php"><div class="menuitem">MyConcept</div></a>
+                    <a href="media.php"><div class="menuitem">Media</div></a>
                     <a href="form.php"><div class="menuitem">Contact</div></a>
                 </div>
             </header>';
