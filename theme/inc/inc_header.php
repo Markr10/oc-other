a55becacd491d1f6f3dd044dfa7c62af5db70fd6
RonSmit <ronsmit92@gmail.com>
2014-01-06 11:40:46 +0100

    De includes linken nu naar index.php ipv index.html

--- theme/inc/inc_header.php
+++ theme/inc/inc_header.php
@@ -1,8 +1,8 @@
 <?php
 echo '<header id="header">
                 <div class="menuspacer">
-                    <a href="index.html"><div id="myconcept"></div></a>
-                    <a href="index.html"><div class="menuitem">Home</div></a>
+                    <a href="index.php"><div id="myconcept"></div></a>
+                    <a href="index.php"><div class="menuitem">Home</div></a>
                     <a href="onderwijs.html"><div class="menuitem">Onderwijs</div></a>
                     <a href="myconcept.html"><div class="menuitem">MyConcept</div></a>
                     <a href="media.html"><div class="menuitem">Media</div></a>
--- theme/inc/inc_submenu.php
+++ theme/inc/inc_submenu.php
@@ -2,7 +2,7 @@
 echo '<div id="submenuholder">
                 <div id="submenu">
                     <div id="submenuspacer">
-                        <a href="index.html"><div class="submenuitem">Home</div></a>
+                        <a href="index.php"><div class="submenuitem">Home</div></a>
                         <a href="onderwijs.html"><div class="submenuitem">Onderwijs</div></a>
                         <a href="myconcept.html"><div class="submenuitem">MyConcept</div></a>
                         <a href="media.html"><div class="submenuitem">Media</div></a>
