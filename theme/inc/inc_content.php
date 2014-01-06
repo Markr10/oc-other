0bc8d95a3519687c6f619abf15fd8c40a213c57e
RonSmit <ronsmit92@gmail.com>
2014-01-06 09:51:01 +0100

    php includes, yo.

---
+++ theme/inc/inc_content.php
@@ -0,0 +1,12 @@
+<?php
+echo '<div id="myconceptcontent">
+                <h1>Discover</h1>
+                Let\'s explore the outside.
+                <div id="myconcepticons">
+                    <a href="#loep"><img src="images/loep.png" alt="loep"></a>
+                    <a href="#box"><img src="images/doos.png" alt="box"></a>
+                    <a href="#erlenmeyer"><img src="images/erlenmeyer.png" alt="erlenmeyer"></a>
+                    <a href="#beeldscherm"><img src="images/beeldscherm.png" alt="beeldscherm"></a>
+                </div>
+            </div>';
+?>
---
+++ theme/inc/inc_header.php
@@ -0,0 +1,12 @@
+<?php
+echo '<header id="header">
+                <div class="menuspacer">
+                    <a href="index.html"><div id="myconcept"></div></a>
+                    <a href="index.html"><div class="menuitem">Home</div></a>
+                    <a href="onderwijs.html"><div class="menuitem">Onderwijs</div></a>
+                    <a href="myconcept.html"><div class="menuitem">MyConcept</div></a>
+                    <a href="media.html"><div class="menuitem">Media</div></a>
+                    <a href="form.php"><div class="menuitem">Contact</div></a>
+                </div>
+            </header>';
+?>
