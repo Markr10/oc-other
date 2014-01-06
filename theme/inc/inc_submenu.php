bf9ea16f6b408dee037e1ba96c5119b791c12965
RonSmit <ronsmit92@gmail.com>
2014-01-06 12:07:29 +0100

    De juiste URL's toegevoegd

--- theme/inc/inc_submenu.php
+++ theme/inc/inc_submenu.php
@@ -3,9 +3,9 @@ echo '<div id="submenuholder">
                 <div id="submenu">
                     <div id="submenuspacer">
                         <a href="index.php"><div class="submenuitem">Home</div></a>
-                        <a href="onderwijs.html"><div class="submenuitem">Onderwijs</div></a>
-                        <a href="myconcept.html"><div class="submenuitem">MyConcept</div></a>
-                        <a href="media.html"><div class="submenuitem">Media</div></a>
+                        <a href="onderwijs.php"><div class="submenuitem">Onderwijs</div></a>
+                        <a href="myconcept.php"><div class="submenuitem">MyConcept</div></a>
+                        <a href="media.php"><div class="submenuitem">Media</div></a>
                         <a href="form.php"><div class="submenuitem">Contact</div></a>
                     </div>
                 </div>
